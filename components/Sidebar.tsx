8b2033b1b77ebbd0825cc0a61fdc7713519b07b6
Samson Ibironke <samsonibironke2@gmail.com>
2025-03-20 17:16:14 +0100

    implement Mobile Navbar commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = ( {user} : SiderbarProps ) => {
     return (
         <section className="sidebar">
             <nav className="flex flex-col gap-4">
-                <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+                <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
                     <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon logo" className="size-[24px] max-xl:size-14"/>
                     <h1 className="sidebar-logo">Horizon</h1>
                 </Link>
@@ -22,11 +22,18 @@ const Sidebar = ( {user} : SiderbarProps ) => {
 
                      return (
                         <Link href={item.route} key={item.label} className={cn ('sidebar-link', { 'bg-gradient' : isActive })}>
-                            {item.label}
+                            <div className="relative size-6">
+                                <Image src={item.imgURL} alt={item.label} fill className={cn({"brightness-[3] invert-0" : isActive})}/>
+                            </div>
+                            <p className={cn("sidebar-label", {"!text-white" : isActive})}>
+                                {item.label}
+                            </p>
                         </Link>
                     )
                 })}
+                USER
             </nav>
+            FOOTER
         </section>
     )
 }
